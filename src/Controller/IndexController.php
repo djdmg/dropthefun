12d9f48223dc5b3b322a05e6af63d2c9e2e335b1
djdmg <dimitrigrard@gmail.com>
2024-09-24 14:53:13 +0200

    Update IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,71 +5,89 @@ namespace App\Controller;
 use App\Service\BlogPostApiClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'blog_index')]
-    public function index(BlogPostApiClient $apiClient): Response
+    public function index(BlogPostApiClient $apiClient, CacheInterface $cache): Response
     {
-        $allPosts=$apiClient->getOnlineBlogPosts(1,5);
+        $cacheKey = 'blog_posts_page_1_limit_5';
 
+        $allPosts = $cache->get($cacheKey, function (ItemInterface $item) use ($apiClient) {
+            $item->expiresAfter(1800); // 1800 secondes = 30 minutes
+            return $apiClient->getOnlineBlogPosts(1, 5);
+        });
 
         return $this->render('index/index.html.twig', [
-            'articles' => $allPosts->getData(), 'pagination' => $allPosts->getPagination()
+            'articles'   => $allPosts->getData(),
+            'pagination' => $allPosts->getPagination(),
         ]);
     }
 
     #[Route('/fr/articles/{page}', name: 'blog_indexfr2')]
-    public function blog_index2(BlogPostApiClient $apiClient,int $page): Response
+    public function blog_index2(BlogPostApiClient $apiClient, CacheInterface $cache, int $page): Response
     {
-        $allPosts=$apiClient->getOnlineBlogPosts($page,5);
+        $cacheKey = sprintf('blog_posts_fr_page_%d_limit_5', $page);
 
-        ;
+        $allPosts = $cache->get($cacheKey, function (ItemInterface $item) use ($apiClient, $page) {
+            $item->expiresAfter(1800);
+            return $apiClient->getOnlineBlogPosts($page, 5);
+        });
+
         return $this->render('index/indexFR.html.twig', [
-            'articles' => $allPosts->getData(), 'pagination' => $allPosts->getPagination()
+            'articles'   => $allPosts->getData(),
+            'pagination' => $allPosts->getPagination(),
         ]);
     }
 
-
     #[Route('/en/articles/{page}', name: 'blog_index2')]
-    public function blog_index2fr(BlogPostApiClient $apiClient,int $page): Response
+    public function blog_index2fr(BlogPostApiClient $apiClient, CacheInterface $cache, int $page): Response
     {
-        $allPosts=$apiClient->getOnlineBlogPosts($page,5);
+        $cacheKey = sprintf('blog_posts_en_page_%d_limit_5', $page);
 
-      ;
+        $allPosts = $cache->get($cacheKey, function (ItemInterface $item) use ($apiClient, $page) {
+            $item->expiresAfter(1800);
+            return $apiClient->getOnlineBlogPosts($page, 5);
+        });
+
         return $this->render('index/index.html.twig', [
-            'articles' => $allPosts->getData(), 'pagination' => $allPosts->getPagination()
+            'articles'   => $allPosts->getData(),
+            'pagination' => $allPosts->getPagination(),
         ]);
     }
 
     #[Route('/en/', name: 'index_principalgi')]
-    public function blog_index(BlogPostApiClient $apiClient): Response
+    public function blog_index(BlogPostApiClient $apiClient, CacheInterface $cache): Response
     {
-        $allPosts=$apiClient->getOnlineBlogPosts(1,5);
+        $cacheKey = 'blog_posts_en_page_1_limit_5';
 
+        $allPosts = $cache->get($cacheKey, function (ItemInterface $item) use ($apiClient) {
+            $item->expiresAfter(1800);
+            return $apiClient->getOnlineBlogPosts(1, 5);
+        });
 
         return $this->render('index/index.html.twig', [
-            'articles' => $allPosts->getData(), 'pagination' => $allPosts->getPagination()
+            'articles'   => $allPosts->getData(),
+            'pagination' => $allPosts->getPagination(),
         ]);
     }
 
     #[Route('/fr/', name: 'blog_index_fr')]
-    public function indexfr(BlogPostApiClient $apiClient): Response
+    public function indexfr(BlogPostApiClient $apiClient, CacheInterface $cache): Response
     {
-        $allPosts=$apiClient->getOnlineBlogPosts(1,5);
+        $cacheKey = 'blog_posts_fr_page_1_limit_5';
+
+        $allPosts = $cache->get($cacheKey, function (ItemInterface $item) use ($apiClient) {
+            $item->expiresAfter(1800);
+            return $apiClient->getOnlineBlogPosts(1, 5);
+        });
 
         return $this->render('index/indexFR.html.twig', [
-            'articles' => $allPosts->getData(),'pagination' => $allPosts->getPagination(),
+            'articles'   => $allPosts->getData(),
+            'pagination' => $allPosts->getPagination(),
         ]);
     }
-
-
-
-
-
-
-
-
-
 }
