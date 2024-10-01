d8499971581825a22f0839f88d1a01363e6bf168
djdmg <dimitrigrard@gmail.com>
2024-10-01 11:14:49 +0200

    Update IndexController.php
    Redirection automatique en fonction de la langue de l'utilisateur

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,12 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends AbstractController
 {
+    /**
+     * Route principale qui redirige en fonction de la langue préférée de l'utilisateur.
+     * Cette route sert principalement la version anglaise.
+     */
     #[Route('/', name: 'blog_index')]
-    public function index(BlogPostApiClient $apiClient, CacheInterface $cache): Response
+    public function index(Request $request, BlogPostApiClient $apiClient, CacheInterface $cache): Response
     {
+        // Vérifier si la langue est déjà stockée dans la session
+        $session = $request->getSession();
+        if ($session->has('preferred_language')) {
+            $preferredLanguage = $session->get('preferred_language');
+        } else {
+            // Détection de la langue préférée de l'utilisateur
+            $preferredLanguage = $request->getPreferredLanguage(['fr', 'en']);
+            $session->set('preferred_language', $preferredLanguage);
+        }
+
+        if ($preferredLanguage === 'fr') {
+            // Redirection vers la version française
+            return $this->redirectToRoute('blog_index_fr');
+        }
+
+        // Si la langue préférée n'est pas 'fr', servir la version anglaise
         $allPosts = $apiClient->getOnlineBlogPosts($cache, 1, 5);
 
         return $this->render('index/index.html.twig', [
@@ -21,45 +42,55 @@ class IndexController extends AbstractController
         ]);
     }
 
-    #[Route('/fr/articles/{page}', name: 'blog_indexfr2')]
-    public function blog_index2(BlogPostApiClient $apiClient, CacheInterface $cache, int $page): Response
-    {
-        $allPosts = $apiClient->getOnlineBlogPosts($cache, $page, 5);
-
-        return $this->render('index/indexFR.html.twig', [
-            'articles'   => $allPosts->getData(),
-            'pagination' => $allPosts->getPagination(),
-        ]);
-    }
-
-    #[Route('/en/articles/{page}', name: 'blog_index2')]
-    public function blog_index2fr(BlogPostApiClient $apiClient, CacheInterface $cache, int $page): Response
-    {
-        $allPosts = $apiClient->getOnlineBlogPosts($cache, $page, 5);
-
-        return $this->render('index/index.html.twig', [
-            'articles'   => $allPosts->getData(),
-            'pagination' => $allPosts->getPagination(),
-        ]);
-    }
-
-    #[Route('/en/', name: 'index_principalgi')]
-    public function blog_index(BlogPostApiClient $apiClient, CacheInterface $cache): Response
-    {
-        $allPosts = $apiClient->getOnlineBlogPosts($cache, 1, 5);
-
-        return $this->render('index/index.html.twig', [
-            'articles'   => $allPosts->getData(),
-            'pagination' => $allPosts->getPagination(),
-        ]);
-    }
-
+    /**
+     * Route pour la version française du site.
+     */
     #[Route('/fr/', name: 'blog_index_fr')]
-    public function indexfr(BlogPostApiClient $apiClient, CacheInterface $cache): Response
+    public function indexFr(Request $request, BlogPostApiClient $apiClient, CacheInterface $cache): Response
     {
+        // Mettre à jour la langue préférée dans la session
+        $session = $request->getSession();
+        $session->set('preferred_language', 'fr');
+
         $allPosts = $apiClient->getOnlineBlogPosts($cache, 1, 5);
 
         return $this->render('index/indexFR.html.twig', [
+            'articles'   => $allPosts->getData(),
+            'pagination' => $allPosts->getPagination(),
+        ]);
+    }
+
+    /**
+     * Route pour les articles en français avec pagination.
+     */
+    #[Route('/fr/articles/{page}', name: 'blog_index_fr_articles', requirements: ['page' => '\d+'])]
+    public function blogIndexFrArticles(BlogPostApiClient $apiClient, CacheInterface $cache, int $page, Request $request): Response
+    {
+        // Mettre à jour la langue préférée dans la session
+        $session = $request->getSession();
+        $session->set('preferred_language', 'fr');
+
+        $allPosts = $apiClient->getOnlineBlogPosts($cache, $page, 5);
+
+        return $this->render('index/indexFR.html.twig', [
+            'articles'   => $allPosts->getData(),
+            'pagination' => $allPosts->getPagination(),
+        ]);
+    }
+
+    /**
+     * Route pour les articles en anglais avec pagination.
+     */
+    #[Route('/articles/{page}', name: 'blog_index_en_articles', requirements: ['page' => '\d+'])]
+    public function blogIndexEnArticles(BlogPostApiClient $apiClient, CacheInterface $cache, int $page, Request $request): Response
+    {
+        // Mettre à jour la langue préférée dans la session
+        $session = $request->getSession();
+        $session->set('preferred_language', 'en');
+
+        $allPosts = $apiClient->getOnlineBlogPosts($cache, $page, 5);
+
+        return $this->render('index/index.html.twig', [
             'articles'   => $allPosts->getData(),
             'pagination' => $allPosts->getPagination(),
         ]);
